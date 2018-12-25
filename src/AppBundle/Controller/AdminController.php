fbc7513a019d20433bc776e719b99d58dd0a210f
Elie <eliebismuth91@gmail.com>
2018-12-25 16:10:49 +0100

    List Users and delete user

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -15,25 +15,49 @@ class AdminController extends Controller
 
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+
         return $this->render('admin/index.html.twig');
     }
 
+    public function showAction( Request $request, $id)
+    {
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $userManager->findUserBy(array('id' => $id));
+
+        return $this->render('user/show.html.twig', array(
+            'user'           => $user,
+
+        ));
+    }
 
     public function listAction(Request $request)
     {
-        // Pour récupérer le service UserManager du bundle
-        $userManager = $this->get('fos_user.user_manager');
 
-        // Pour récupérer la liste de tous les utilisateurs
+        $userManager = $this->get('fos_user.user_manager');
         $users = $userManager->findUsers();
 
         return $this->render('user/list.html.twig', array(
             'users'           => $users,
 
         ));
+    }
 
+    public function deleteAction(Request $request, $id)
+    {
 
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $userManager->findUserBy(array('id' => $id));
+        $form = $this->get('form.factory')->create();
 
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+
+            $userManager->deleteUser($user);
+            return $this->redirectToRoute('app_user_list');
+        }
+
+        return $this->render('user/delete.html.twig', array(
+            'user' => $user,
+            'form'   => $form->createView(),
+        ));
     }
 }
